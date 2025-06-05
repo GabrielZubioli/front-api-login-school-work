4e2b603d46a8ac50d213560e34818a0f5b376ad4
Gabriel Zubioli <138734480+GabrielZubioli@users.noreply.github.com>
2025-06-05 15:44:03 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -64,7 +64,7 @@ async function cadastrarUsuario() {
 function formatarData(dataISO) {
   const data = new Date(dataISO);
   const dia = String(data.getDate()).padStart(2, "0");
-  const mes = String(data.getMonth() + 1).padStart(2, "0"); // mês começa do 0
+  const mes = String(data.getMonth() + 1).padStart(2, "0");
   const ano = data.getFullYear();
   const horas = String(data.getHours()).padStart(2, "0");
   const minutos = String(data.getMinutes()).padStart(2, "0");
